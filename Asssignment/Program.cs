00b31509ce0d5ea4992cee5567c28e143af210d8
Hassan <hassan.farahat@su.edu.eg>
2024-12-12 21:04:05 +0200

    Add project files.

---
+++ Asssignment/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Asssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Write a program that allows the user to enter a number then print it.
+
+
+            //Console.WriteLine("Please Enter your Number");
+            //int num = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("The number is" + num);
+            #endregion
+
+        }
+    }
+}
