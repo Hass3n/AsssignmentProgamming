4d6ee1b67871ccaa36dc52a55081134bd19ddcf9
Hassan <hassan.farahat@su.edu.eg>
2024-12-12 21:06:22 +0200

    solve Q2

--- Asssignment/Program.cs
+++ Asssignment/Program.cs
@@ -11,7 +11,28 @@
             //int num = Convert.ToInt32(Console.ReadLine());
 
             //Console.WriteLine("The number is" + num);
-            #endregion 
+            #endregion
+
+
+            #region Write C# program that Assigning one value type variable to another and modifying the value of one variable and mention what will happen
+
+
+            //int x = 100;
+            //int y = 50;
+            ////Assign x=y
+            //x = y;
+            //Console.WriteLine("the value of x " + x);
+            //Console.WriteLine("the value of y" + y);
+
+            //// after modifcation  Y
+            //y = 10;
+            //Console.WriteLine("the value of x after Modify" + x);
+            //Console.WriteLine("the value of y after modify " + y);
+
+            // X is not Upadte Beacause x = y it is copy value but not copy References
+
+
+            #endregion
 
         }
     }
